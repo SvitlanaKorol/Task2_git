bc089de703fe4e01ef0869ff3a238702939afb2c
Svitlana Korol <Svitlana_Korol@epam.com>
2021-06-30 15:45:25 +0300

    first commit

---
+++ Korol_Mykhailovskyi/song.rb
@@ -0,0 +1,6 @@
+I believe in love, it's all we got
+Love has no boundaries, costs nothing to touch
+War makes money, cancer sleeps
+Curled up in my father and that means something to me
+Churches and dictators, politics and papers
+Everything crumbles sooner or later
